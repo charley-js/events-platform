697de2201eb633dac3b8bd688a42537f7ec4b33b
charley-js <charleycrowson1997@gmail.com>
2025-01-14 22:10:15 +0000

    Add basic user and event test data

---
+++ database/data/test-data/events.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    title: "Great Park Run",
+    description:
+      "20th anniversary of The Great Park Run, all in the name of charity. Join us , get fit, do something big.",
+    date: "13th February 2025, 10:00AM",
+    created_at: "14th January 2025, 21:56PM",
+  },
+  {
+    title: "90's Dance Party",
+    description:
+      "Relive the night life on the 90's , a one time event organised by us for you, featuring your favourite oldskool anthems and DJ's.",
+    date: " 30th June 2025, 20:00PM",
+    created_at: "14th January 2025, 22:07PM",
+  },
+];
---
+++ database/data/test-data/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    username: "john",
+    email: "johndoe@gmail.com",
+    password: "password",
+  },
+  {
+    username: "simon",
+    email: "simonsays@gmail.com",
+    password: "1234",
+  },
+  {
+    username: "ben",
+    email: "benson1@gmail.com",
+    password: "abc",
+  },
+];
