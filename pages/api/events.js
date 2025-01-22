db0a63e5f13d9b205696942f5db2e220c9876fd9
Charley Crowson <charleycrowson1997@gmail.com>
2025-01-22 15:20:51 +0000

    Create GET /event?_id= endpoint, to retrieve a specific event by its id

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,14 +1,20 @@
 import { connect } from "../../database/connection";
+import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
   const client = await connect();
   const db = client.db();
   const events = db.collection("events");
   const { method } = req;
+  const { _id } = req.query;
 
   try {
     if (method === "GET") {
-      await getAllEvents(events, res);
+      if (_id) {
+        await getEvent(events, _id, res);
+      } else {
+        await getAllEvents(events, res);
+      }
     } else {
       res.status(405).json({ message: "Invalid method" });
     }
@@ -27,5 +33,21 @@ async function getAllEvents(events, res) {
   } catch (error) {
     console.error("Error fetching events.", error.message);
     res.status(500).json({ message: "Error fetching events." });
+  }
+}
+
+async function getEvent(events, _id, res) {
+  try {
+    if (!ObjectId.isValid(_id)) {
+      return res.status(400).json({ message: "Invalid ID format" });
+    }
+    const event = await events.findOne({ _id: new ObjectId(_id) });
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+    res.status(200).json(event);
+  } catch (error) {
+    console.error("Error fetching event: ", error.message);
+    res.status(500).json({ message: "Error fetching event" });
   }
 }
