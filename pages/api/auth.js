31cb7b6abcfbcd4e7105c8bfdcddfaf4b37efe46
Charley Crowson <charleycrowson1997@gmail.com>
2025-01-31 01:16:08 +0000

    Google Auth added to POST /api/auth endpoint, checking the google id sent in the response against the google id saved in the database at time of signup

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,15 +1,22 @@
 import { connect } from "../../database/connection";
 import bcrypt from "bcrypt";
+import { google } from "googleapis";
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+  process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI
+);
 
 export default async function handler(req, res) {
   const client = await connect();
   const db = client.db();
   const users = db.collection("users");
-  const { username, password } = req.body;
+  const { username, password, googleToken } = req.body;
 
   try {
-    if (!username || !password) {
-      return res.status(400).json({ message: "Username and password required" });
+    if (!username || !password || !googleToken) {
+      return res.status(400).json({ message: "Username, password and Google authentication required" });
     }
     const user = await users.findOne({ username });
     if (!user) {
@@ -18,6 +25,14 @@ export default async function handler(req, res) {
     const verify = await bcrypt.compare(password, user.password);
     if (!verify) {
       return res.status(401).json({ message: "Invalid username or password" });
+    }
+    const token = await oauth2Client.verifyIdToken({
+      idToken: googleToken,
+      audience: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    });
+    const payload = token.getPayload();
+    if (!payload || user.googleId !== payload.sub) {
+      return res.status(401).json({ message: "Google authentication failed" });
     }
     return res.status(200).json({ message: "Authentication successful", userId: user._id.toString() });
   } catch (error) {
