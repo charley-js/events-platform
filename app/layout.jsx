9a511350777026c648be3652895879819c5499a2
charley-js <charleycrowson1997@gmail.com>
2025-02-01 22:54:22 +0000

    Create NavBar component with Basic Styling and render on pages that arent /login or /signup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "../components/ui/provider";
+import NavBar from "../components/NavBar";
 
 export const Metadata = {
   title: "Events Platform",
@@ -12,7 +14,10 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning={true}>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
         <body>
-          <Provider>{children}</Provider>
+          <Provider>
+            <NavBar />
+            <main>{children}</main>
+          </Provider>
         </body>
       </GoogleOAuthProvider>
     </html>
---
+++ components/NavBar.jsx
@@ -0,0 +1,28 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import NextLink from "next/link";
+
+export default function NavBar() {
+  const pathname = usePathname();
+
+  if (pathname === "/login" || pathname === "/signup") return null;
+
+  return (
+    <Box bg="teal" px={4} py={3} mb={4}>
+      <Flex justify={"space-between"} align="center" width="100%">
+        <Text fontSize="xl" fontWeight="bold" color="white">
+          Events Platform
+        </Text>
+        <Flex gap={10} justify="center" align="center" flex="1">
+          <Link as={NextLink} href="/" color="white">
+            Dashboard
+          </Link>
+          <Link as={NextLink} href="/events" color="white">
+            Events
+          </Link>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
