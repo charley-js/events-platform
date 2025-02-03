f72306faf24c5c286e2b02aad22f0ad9b111086c
Charley Crowson <charleycrowson1997@gmail.com>
2025-02-03 16:40:11 +0000

    Added additional event test data

--- database/data/test-data/events.js
+++ database/data/test-data/events.js
@@ -1,8 +1,6 @@
-import { ObjectId } from "mongodb";
-
 export default [
   {
-    _id: new ObjectId("60f8c0f8b8a5ca401f2fbb4e"),
+    _id: "60f8c0f8b8a5ca401f2fbb4e",
     title: "Great Park Run",
     description:
       "20th anniversary of The Great Park Run, all in the name of charity. Join us , get fit, do something big.",
@@ -12,7 +10,7 @@ export default [
     created_at: "2025-01-14 21:56:00",
   },
   {
-    _id: new ObjectId("60f8c0f8b8a5ca401f2fbb5f"),
+    _id: "60f8c0f8b8a5ca401f2fbb5f",
     title: "90's Dance Party",
     description:
       "Relive the night life on the 90's , a one time event organised by us for you, featuring your favourite oldskool anthems and DJ's.",
@@ -22,7 +20,7 @@ export default [
     created_at: "2025-01-14 22:07:00",
   },
   {
-    _id: new ObjectId("60f8c0f8b8a5ca401f2fbb6d"),
+    _id: "60f8c0f8b8a5ca401f2fbb6d",
     title: "Christmas Light Switch On",
     description:
       "Join the community in the town square as we kick off the festive period with our annual christmas light switch!",
@@ -32,12 +30,30 @@ export default [
     created_at: "2025-01-15 19:28:00",
   },
   {
-    _id: new ObjectId("60f8c0f8b8a5ca401f2fbb7b"),
+    _id: "60f8c0f8b8a5ca401f2fbb7b",
     title: "Cinderella At The Pantomime",
     description: "The famous fairy tale of Cinderella, retold as a theatre production here at our local pantomime!",
     date: "2025-05-05 16:00:00",
     category: "Theatre",
     attendees: [],
     created_at: "2025-01-14 22:07:00",
+  },
+  {
+    _id: "67a0f09f3539282d70f8c907",
+    title: "London New Years Firework Display",
+    description: "The annual firework display on the River Thames, ending the year with a bang!",
+    date: "2025-12-31T23:00",
+    category: "Festivities",
+    created_at: "1738600607559",
+    attendees: [],
+  },
+  {
+    _id: "67a0f0e43539282d70f8c908",
+    title: "Museum Open Day",
+    description: "Free entry all day for one day only during the summer holidays!",
+    date: "2025-07-13T13:00",
+    category: "Activities",
+    created_at: "2025-01-15 19:28:00",
+    attendees: [],
   },
 ];
