653ce4cac30e332b81151527bd26d52ebb250d98
Charley Crowson <charleycrowson1997@gmail.com>
2025-02-14 14:27:43 +0000

    Make Events Page root

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,7 @@
 "use client";
 import React from "react";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "../components/ui/provider";
 import NavBar from "../components/NavBar";
-import dynamic from "next/dynamic";
-
-// const Dashboard = dynamic(() => import("../app/page"), { ssr: false });
-// const SignupPage = dynamic(() => import("../app/signup/page"), { ssr: false });
-// const LoginPage = dynamic(() => import("../app/login/page"), { ssr: false });
-// const CreateEventPage = dynamic(() => import("../app/create-event/page"), { ssr: false });
-// const EditEventPage = dynamic(() => import("../app/edit-event/[id]/page"), { ssr: false });
-// const EventDetailsPage = dynamic(() => import("../app/events/[id]/page"), { ssr: false });
-// const EventsPage = dynamic(() => import("../app/events/page"), { ssr: false });
 
 export const Metadata = {
   title: "Events Platform",
@@ -21,14 +11,12 @@ export const Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
-        <body>
-          <Provider>
-            <NavBar />
-            <main>{children}</main>
-          </Provider>
-        </body>
-      </GoogleOAuthProvider>
+      <body>
+        <Provider>
+          <NavBar />
+          <main>{children}</main>
+        </Provider>
+      </body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,37 +1,44 @@
 "use client";
-import { React, useEffect, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Card, CardBody, Heading, Box, Stack, Text, HStack, Center, Spinner } from "@chakra-ui/react";
-import { Avatar } from "../components/ui/avatar";
+import EventCard from "../components/eventCard.jsx";
+import { Heading, Button, SimpleGrid, Box, Flex, Center, Spinner } from "@chakra-ui/react";
 import Cookies from "js-cookie";
 
-export default function Dashboard() {
-  const [user, setUser] = useState({});
+export default function EventsPage() {
   const [events, setEvents] = useState([]);
+  const [isMod, setIsMod] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [buttonLoading, setButtonLoading] = useState(false);
   const router = useRouter();
 
-  useEffect(() => {
-    const userId = Cookies.get("userId");
-    if (!userId) {
-      router.push("/login");
-      return;
-    }
-    fetch(`/api/users?_id=${userId}`)
-      .then((res) => res.json())
-      .then((data) => setUser(data))
-      .catch(() => router.push("/login"));
-
-    fetch(`/api/events?userId=${userId}`)
-      .then((res) => res.json())
-      .then((data) => setEvents(data))
-      .catch((err) => {
-        setEvents([]);
-      })
-      .finally(() => {
-        setLoading(false);
+  function fetchEvents() {
+    if (typeof window !== "undefined") {
+      const userIsMod = Cookies.get("isMod");
+      if (userIsMod === "true") {
+        setIsMod(true);
+      }
+      fetch("/api/events").then((res) => {
+        return res
+          .json()
+          .then((data) => {
+            setEvents(data);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
       });
+    }
+  }
+
+  useEffect(() => {
+    fetchEvents();
   }, []);
+
+  function handleCreateEvent() {
+    setButtonLoading(true);
+    router.push("/create-event");
+  }
 
   if (loading) {
     return (
@@ -40,63 +47,26 @@ export default function Dashboard() {
       </Center>
     );
   }
-  return (
-    <Box p={6} maxW="800px" mx="auto">
-      <Card.Root mb={6} p={6} borderRadius="xl" boxShadow="lg" bg={"gray.950"}>
-        <CardBody>
-          <HStack mb={4} gap={3}>
-            <Avatar size={"2xl"} name={user.username} src="" colorPalette={"blue"} />
-            <Stack spacing={1} ml={4}>
-              <Heading size="lg">Welcome, {user.username}</Heading>
-              <Text color="gray.500">{user.email}</Text>
-              <Text color="gray.300" fontSize="sm">
-                Attending {user.events ? user.events.length : 0} events
-              </Text>
-            </Stack>
-          </HStack>
-        </CardBody>
-      </Card.Root>
 
-      <Card.Root p={5} borderRadius="lg" boxShadow="md" bg="gray.950">
-        <CardBody>
-          <Heading size="md" mb={4}>
-            Your Events
-          </Heading>
-          {events.length > 0 ? (
-            events.map((event) => (
-              <Card.Root
-                key={event._id}
-                mb={4}
-                p={4}
-                borderRadius="lg"
-                boxShadow="sm"
-                transition="transform 0.2s ease-in-out"
-                _hover={{ transform: "scale(1.02)", boxShadow: "2xl" }}
-                borderLeft="6px solid #D52929"
-                bg={"gray.900"}
-              >
-                <CardBody>
-                  <Heading size="md" mb={2}>
-                    {event.title}
-                  </Heading>
-                  <Text fontSize="sm" color="gray.500">
-                    {event.category}
-                  </Text>
-                  <Text mt={2}>{event.description}</Text>
-                  <Text fontSize="sm" mt={2}>
-                    Date: {new Date(event.date).toLocaleDateString()}
-                  </Text>
-                  <Text fontSize="sm" mt={1}>
-                    {event.attendees.length} Attending
-                  </Text>
-                </CardBody>
-              </Card.Root>
-            ))
-          ) : (
-            <Text color="gray.500">No events available.</Text>
-          )}
-        </CardBody>
-      </Card.Root>
-    </Box>
+  return (
+    <div>
+      <Flex justify="center" align="center" mb={8}>
+        <Heading size={"2xl"} textAlign={"center"} flex="1" ml={28}>
+          All Events
+        </Heading>
+        {isMod && (
+          <Button loading={buttonLoading} loadingText={"Loading..."} onClick={handleCreateEvent} ml="auto" right={"8"}>
+            Create Event
+          </Button>
+        )}
+      </Flex>
+      <SimpleGrid columns={{ base: 1, sm: 2, md: 3 }} spacing={4} ml={27}>
+        {events.map((event) => (
+          <Box key={event._id}>
+            <EventCard event={event} isMod={isMod} fetchEvents={fetchEvents} />
+          </Box>
+        ))}
+      </SimpleGrid>
+    </div>
   );
 }
