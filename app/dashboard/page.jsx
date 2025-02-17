07c1e64f1c6d796b249474d57da115e57016e7d8
Charley Crowson <charleycrowson1997@gmail.com>
2025-02-17 23:49:27 +0000

    Add advanced styling to Dashboard page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 import { React, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Card, CardBody, Heading, Box, Stack, Text, HStack, Center, Spinner } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Box, Stack, Text, HStack, Center, Spinner, Image, Separator } from "@chakra-ui/react";
 import { Avatar } from "../../components/ui/avatar";
+import { Tag } from "../../components/ui/tag";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import Cookies from "js-cookie";
 
 export default function Dashboard() {
@@ -41,51 +43,84 @@ export default function Dashboard() {
     );
   }
   return (
-    <Box p={6} maxW="800px" mx="auto">
-      <Card.Root mb={6} p={6} borderRadius="xl" boxShadow="lg" bg={"gray.950"}>
+    <Box p={6}>
+      <Card.Root mb={6} p={2} borderRadius="xl" boxShadow="lg" bg={"gray.950"} maxWidth={"800px"}>
         <CardBody>
           <HStack mb={4} gap={3}>
             <Avatar size={"2xl"} name={user.username} src="" colorPalette={"blue"} />
-            <Stack spacing={1} ml={4}>
-              <Heading size="lg">Welcome, {user.username}</Heading>
-              <Text color="gray.500">{user.email}</Text>
-              <Text color="gray.300" fontSize="sm">
-                Attending {user.events ? user.events.length : 0} events
-              </Text>
-            </Stack>
+            <Heading ml={4} size="3xl">
+              Welcome, {user.username}!
+            </Heading>
           </HStack>
+
+          <Stack ml={24} mt={-7}>
+            <Text>{user.email}</Text>
+            <Tag colorPalette="red" variant="solid" fontSize="2xl" size={"lg"} width={"fit"} mb={6}>
+              Attending {user.events ? user.events.length : 0} events
+            </Tag>
+            <Text fontSize={"sm"} fontStyle={"italic"} opacity={0.5}>
+              Member since {new Date(user.created_at).toLocaleDateString()}.
+            </Text>
+          </Stack>
         </CardBody>
       </Card.Root>
 
       <Card.Root p={5} borderRadius="lg" boxShadow="md" bg="gray.950">
         <CardBody>
-          <Heading size="md" mb={4}>
+          <Heading size="3xl" mb={8} textAlign={"center"}>
             Your Events
           </Heading>
           {events.length > 0 ? (
             events.map((event) => (
               <Card.Root
                 key={event._id}
-                mb={4}
-                p={4}
+                mb={6}
                 borderRadius="lg"
                 boxShadow="sm"
                 transition="transform 0.2s ease-in-out"
                 _hover={{ transform: "scale(1.02)", boxShadow: "2xl" }}
-                borderLeft="6px solid #D52929"
                 bg={"gray.900"}
+                w={"45%"}
+                mx={"auto"}
               >
                 <CardBody>
-                  <Heading size="md" mb={2}>
+                  {event.imageUrl && (
+                    <Image
+                      src={event.imageUrl}
+                      alt={event.title}
+                      borderRadius="md"
+                      mb={3}
+                      maxHeight={100}
+                      htmlWidth={"100%"}
+                    />
+                  )}
+                  <Heading size="lg" mb={2}>
                     {event.title}
                   </Heading>
-                  <Text fontSize="sm" color="gray.500">
+                  <Tag size={"lg"} colorPalette={"red"} variant={"solid"} w={"fit"} mb={4}>
                     {event.category}
+                  </Tag>
+                  <Separator borderColor="gray.600" />
+                  <Text mt={2} mb={4}>
+                    {event.description}
                   </Text>
-                  <Text mt={2}>{event.description}</Text>
-                  <Text fontSize="sm" mt={2}>
-                    Date: {new Date(event.date).toLocaleDateString()}
-                  </Text>
+                  <Box background={"gray.800"} padding={2} borderRadius={20} w={"40%"} mb={4}>
+                    <Text fontSize="sm" opacity={0.7} paddingTop={2}>
+                      📅 {new Date(event.date).toLocaleDateString()}
+                    </Text>
+                    <Text fontSize="md" opacity={0.7}>
+                      ⏰{" "}
+                      {new Date(event.date).toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: true,
+                      })}
+                    </Text>
+                    <Text fontSize="md" opacity={0.7}>
+                      <FaMapMarkerAlt style={{ display: "inline", marginRight: "5px" }} />
+                      {event.venue}
+                    </Text>
+                  </Box>
                   <Text fontSize="sm" mt={1}>
                     {event.attendees.length} Attending
                   </Text>
